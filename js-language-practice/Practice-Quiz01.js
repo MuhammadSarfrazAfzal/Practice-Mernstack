cfba5f3d9963ce4f1f81b0c850b04e7272e2b8a9
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-02-23 01:30:23 +0500

    add assigment and unary operators ,boolean in js question and answer

--- js-language-practice/Practice-Quiz01.js
+++ js-language-practice/Practice-Quiz01.js
@@ -1,11 +1,16 @@
-// What is the value after this code?
+//                  This quiz is from the starting concept of js in which I have learn about types of datatypes,
+//  how to declare a variable , usiong let ,var and const difference and different types of assigment and unary operators.
+
+
+
+// Q no:01 What is the value after this code?
 let age = 23;
 age + 2;
 console.log(age)
 // the value after this code is 25
 
 
-// what is value of avg after this code runs?
+//Q no:02 what is value of avg after this code runs?
 let urdu = 85;
 let eng = 90;
 let math = 100;
@@ -13,3 +18,23 @@ const avg = (85 + 90 + 100)/3;
 console.log(avg);
 // the value of avg is 91.66.
 
+//Q no:03 What is the value of each variable in each line of code?
+let num = 5;
+console.log(num);
+// output:5
+let newNum = num++;
+console.log(newNum);
+// output:6
+newNum = ++newNum;
+console.log(newNum);
+// output:7
+
+// Q no:04 find the errors in following code?
+// let 1age = 5;
+let age1 = 5; //variable name can't start from number.
+// let isPass = True;
+let isPass = true; //True is word not a bollian value.if we want to use bollian value
+// then write true .
+
+// let isPass = 'true';
+let ispass = true;
