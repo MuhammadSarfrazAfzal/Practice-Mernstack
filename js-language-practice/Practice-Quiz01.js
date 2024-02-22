6920665e77cfe89690f359922f1e4320400a20a3
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-02-22 23:42:59 +0500

    solving first quiz of js showing output with reasigning value to variables

---
+++ js-language-practice/Practice-Quiz01.js
@@ -0,0 +1,15 @@
+// What is the value after this code?
+let age = 23;
+age + 2;
+console.log(age)
+// the value after this code is 25
+
+
+// what is value of avg after this code runs?
+let urdu = 85;
+let eng = 90;
+let math = 100;
+const avg = (85 + 90 + 100)/3;
+console.log(avg);
+// the value of avg is 91.66.
+
