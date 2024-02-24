354117e7abf39aaa2789bfb345b3399a4bafa7c2
Muhammad Sarfraz Afzal <sarfrazafzal790@gmail.com>
2024-02-24 13:30:37 +0500

    solving question 1 of Quiz 2

---
+++ js-language-practice/Practice-Quiz02.js
@@ -0,0 +1,13 @@
+//QuestionNo 01: Create a traffic light system that shows what to do 
+// based on color.
+ let lightColor = "Red";
+
+ if(lightColor == "Green"){
+    console.log("Vehical can move!")
+ }
+else if(lightColor == "Yellow"){
+    console.log("start your engine")
+ }
+ else if(lightColor == "Red") {
+    console.log("stop")
+ };
