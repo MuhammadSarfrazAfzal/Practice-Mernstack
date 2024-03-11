0d784a28ee5ee1fdaf402119155836250c1b219d
Muhammad Sarfraz Afzal <sarfrazafzal790@gmail.com>
2024-03-11 22:49:33 +0500

    Solving array problems

---
+++ js-language-practice/Practice-Quiz04.js
@@ -0,0 +1,10 @@
+// Write a program to get first n elements of an arary.[n can bi any positive number].
+// for example: for array [7,9,0,-2] and n=3
+// print [7,9,0]
+let arr1 = [7,9,0,-2];
+console.log(arr1.slice(0,3));
+
+// Write a program to get last n elements of an arary.[n can bi any positive number].
+// for example: for array [7,9,0,-2] and n=3
+// print [9,0,-2]
+console.log(arr1.slice(1,4));
