fb614ec0268afe36478a49bcbdcea68f2cf22777
Muhammad Sarfraz Afzal <sarfrazafzal790@gmail.com>
2024-05-04 11:47:26 +0500

    callback hell created

--- js-language-practice/Asynchronous Nature/Asynchronous.js
+++ js-language-practice/Asynchronous Nature/Asynchronous.js
@@ -8,3 +8,42 @@ function invokesFunction(callback){
     setTimeout(callback,2000);
 }
 invokesFunction(displayText);
+
+// 2. Write a JavaScript program that converts 
+// a callback-based function to a Promise-based function.
+
+function saveData(data,success,failure){
+    let internetSpeed = Math.floor(Math.random()*10)+1;
+    if(internetSpeed>5){
+        success();
+    }
+    else{
+        failure();
+    }
+}
+// Callback hell:
+saveData("Hello: This is data 1",
+    ()=>{
+        console.log("Success:Data 1 is saved");
+        saveData(
+            "Hello: This is data 2",
+            ()=>{
+                console.log("Success:Data 2 is saved")
+                saveData(
+                    "Hello: This is data 3",
+                    ()=>{
+                        console.log("Success:Data 3 is saved")
+                    },
+                    ()=>{
+                        console.log("Failure:Connection is weak")
+                    }
+                )
+            },
+            ()=>{
+                console.log("Failure:Connection is weak")
+            }
+        )
+    },
+    ()=>{
+        console.log("Failure:Connection is weak");
+});
