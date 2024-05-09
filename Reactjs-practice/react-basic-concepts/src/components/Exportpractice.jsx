f65f8fd060dd6ecb2d46dac865e375f37ba91814
Muhammad Sarfraz Afzal <sarfrazafzal790@gmail.com>
2024-05-10 00:13:48 +0500

    Learned fragment and props

---
+++ Reactjs-practice/react-basic-concepts/src/components/Exportpractice.jsx
@@ -0,0 +1,8 @@
+function Exportpractice(){
+    return <h3>export by default function!</h3>
+}
+export default Exportpractice;
+
+export function Exportbyname(){
+    return <h3>export by function name</h3>
+};
---
+++ Reactjs-practice/react-basic-concepts/src/components/Fragments.jsx
@@ -0,0 +1,9 @@
+function Fragments(){
+    return (
+        <>
+        <h1>Using Fragments:</h1>
+        <button>Like it!</button>
+        </>
+    )
+}
+export default Fragments;
