3ac5b43d3ac889a65e60be4f9888f15c18d3ad4e
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-11-06 20:56:03 +0500

    Understanding request and response

--- Express-practice/index.js
+++ Express-practice/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
-let port = 3000
+let port = 8080
 app.listen(port,()=>{
     console.log("app is listening on port" + port)
 })
+app.get('/',(req,res)=>{
+    res.send("<h1>You are on home route</h1>")
+})
+app.get('/contact',(req,res)=>{
+    res.send("<h1>contact will bi provided soon</h1>")
+})
+
+app.get('*',(req,res)=>{
+    res.send("<h1>sorry content not found</h1>")
+})
+// app.use((req,res)=>{
+//     console.log('request recieved')
+//     res.send({name:'sarfraz',address:})
+// })
