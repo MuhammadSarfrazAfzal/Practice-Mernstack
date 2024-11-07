713e78952e88a1233c99942726a2bc34e70db4ad
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-11-07 22:32:10 +0500

    have learned about querry string and request params

--- Express-practice/index.js
+++ Express-practice/index.js
@@ -4,10 +4,13 @@ let port = 8080
 app.listen(port,()=>{
     console.log("app is listening on port" + port)
 })
-app.get('/',(req,res)=>{
-    res.send("<h1>You are on home route</h1>")
+app.get('/:username/:id',(req,res)=>{
+    console.log(req.params)
+    let { username,id} = req.params
+    res.send(`<h1>You are on @${username} acount</h1>`)
 })
 app.get('/contact',(req,res)=>{
+    console.log(req.query)
     res.send("<h1>contact will bi provided soon</h1>")
 })
 
