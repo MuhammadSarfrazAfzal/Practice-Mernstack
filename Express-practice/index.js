35a36661b7b02d04712b66778b7de229bdd06e58
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-11-08 20:47:17 +0500

    Rendering response through ejs template views

--- Express-practice/index.js
+++ Express-practice/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 let port = 8080
+const path = require('path')
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,"/views"))
+
 app.listen(port,()=>{
     console.log("app is listening on port" + port)
 })
-app.get('/:username/:id',(req,res)=>{
-    console.log(req.params)
-    let { username,id} = req.params
-    res.send(`<h1>You are on @${username} acount</h1>`)
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
 })
 app.get('/contact',(req,res)=>{
     console.log(req.query)
